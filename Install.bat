f632b96e8763668229a25cdaf126f2b2de86655d
Herman Brunberg <herman.brunberg@utb.ecutbildning.se>
2022-01-28 11:49:22 +0100

    la till det jag kunde komma på

--- Install.bat
+++ Install.bat
@@ -1,2 +1,1 @@
-npm install jsdoc
-npm install rxjs
+npm install jsdoc && code --install-extension Tobermory.es6-string-html && code --install-extension abusaidm.html-snippets && code --install-extension bashmish.es6-string-css && code --install-extension pranaygp.vscode-css-peek && code --install-extension ritwickdey.live-sass && code --install-extension syler.sass-indented && code --install-extension dbaeumer.jshint && code --install-extension zjcompt.es6-string-javascript && code --install-extension eamodio.gitlens && code --install-extension esbenp.prettier-vscode && code --install-extension ms-dotnettools.csharp && code --install-extension ritwickdey.LiveServer
