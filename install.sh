2651f30cf8ebdb4b63bbca435ae1493befbda730
Herman Brunberg <89459623+Pingu1337@users.noreply.github.com>
2022-01-29 13:18:45 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+sudo apt-get update -y && sudo apt-get upgrade -y
+sudo apt-get install nodejs -y && sudo apt-get install npm -y
+npm install jsdoc
+code --install-extension Tobermory.es6-string-html
+code --install-extension abusaidm.html-snippets
+code --install-extension bashmish.es6-string-css
+code --install-extension pranaygp.vscode-css-peek
+code --install-extension ritwickdey.live-sass
+code --install-extension syler.sass-indented
+code --install-extension dbaeumer.jshint
+code --install-extension zjcompt.es6-string-javascript
+code --install-extension eamodio.gitlens
+code --install-extension esbenp.prettier-vscode
+code --install-extension ms-dotnettools.csharp
+code --install-extension ritwickdey.LiveServer
+code --install-extension formulahendry.auto-rename-tag
+echo Installation complete! 
