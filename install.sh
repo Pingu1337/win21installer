82095a4310918e1504e21e043e461fa019ad59d6
Herman Brunberg <89459623+Pingu1337@users.noreply.github.com>
2022-01-29 13:51:47 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 sudo apt-get update -y && sudo apt-get upgrade -y
-sudo apt-get install nodejs -y && sudo apt-get install npm -y
+sudo apt install nodejs -y && sudo apt install npm -y
 npm install jsdoc
 
-code --user-data-dir --install-extension Tobermory.es6-string-html --force 
-code --user-data-dir --install-extension abusaidm.html-snippets --force 
-code --user-data-dir --install-extension bashmish.es6-string-css --force 
-code --user-data-dir --install-extension pranaygp.vscode-css-peek --force
-code --user-data-dir --install-extension ritwickdey.live-sass --force
-code --user-data-dir --install-extension syler.sass-indented --force
-code --user-data-dir --install-extension dbaeumer.jshint --force
-code --user-data-dir --install-extension zjcompt.es6-string-javascript --force
-code --user-data-dir --install-extension eamodio.gitlens --force
-code --user-data-dir --install-extension esbenp.prettier-vscode --force
-code --user-data-dir --install-extension ms-dotnettools.csharp --force
-code --user-data-dir --install-extension ritwickdey.LiveServer --force
-code --user-data-dir --install-extension formulahendry.auto-rename-tag --force
+code --install-extension Tobermory.es6-string-html --force 
+code --install-extension abusaidm.html-snippets --force 
+code --install-extension bashmish.es6-string-css --force 
+code --install-extension pranaygp.vscode-css-peek --force
+code --install-extension ritwickdey.live-sass --force
+code --install-extension syler.sass-indented --force
+code --install-extension dbaeumer.jshint --force
+code --install-extension zjcompt.es6-string-javascript --force
+code --install-extension eamodio.gitlens --force
+code --install-extension esbenp.prettier-vscode --force
+code --install-extension ms-dotnettools.csharp --force
+code --install-extension ritwickdey.LiveServer --force
+code --install-extension formulahendry.auto-rename-tag --force
 echo Installation complete! 
